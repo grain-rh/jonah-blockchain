b4fc153986f3c372e800f44d1631fca3aa8ce817
Ubuntu <ruairi@hyperledger-server.tscc4x2ddltefpbr0myoafomuc.bx.internal.cloudapp.net>
2018-06-07 13:43:42 +0000

    adding contracts

---
+++ contracts/graindiscovery/lib/logic.js
@@ -0,0 +1,80 @@
+
+/* global getAssetRegistry getParticipantRegistry */
+
+/**
+ * Fill an ask
+ * @param {org.graindiscovery.fillAsk} ask - the offer
+ * @transaction
+ */
+async function fillAsk(ask) {
+  
+  let grain = ask.grain;
+  let grainOwner = grain.owner;
+  let asker = ask.member;
+  
+  console.log('#### Askers Balance: ' + ask.member.balance);
+  console.log('#### Grain Owner balance: ' + ask.grain.owner.balance);
+  console.log('#### Grain Value: ' + ask.grain.value);
+  console.log('#### Grain Owner Balance: ' + ask.member.balance);
+  
+  if (grain.state !== 'FOR_SALE') {
+        throw new Error('Grain is not FOR SALE');
+  }
+  
+  if (grain.value > asker.balance)
+  {
+    throw new Error('Insufficent balance for transaction');
+  }
+  
+  grainOwner.balance = grainOwner.balance + grain.value;
+  asker.balance = asker.balance - grain.value;
+  grain.owner = asker;
+  grain.state = 'SOLD';
+
+  const grainRegistry = await getAssetRegistry('org.graindiscovery.Grain');
+    await grainRegistry.update(grain);
+  
+  const userRegistry = await getParticipantRegistry('org.graindiscovery.Member');
+    await userRegistry.update(asker);
+    await userRegistry.update(grainOwner);
+    
+}
+
+/**
+ * Make an Offer for a VehicleListing
+ * @param {org.graindiscovery.fillBid} fill - the offer
+ * @transaction
+ */
+async function fillBid(fill) {
+  let grain = fill.grain;
+  let grainSeller = fill.member;
+  let bid = fill.bid;
+  let bidder = fill.bid.owner;
+  
+  if (bid.state !== 'FOR_SALE') {
+        throw new Error('Bid Not Active');
+	}
+  
+  if (bidder.balance < bid.value)
+  {
+    throw new Error('Insufficent balance for transaction');
+  }
+  
+   grainSeller.balance = grainSeller.balance + bid.value;
+   bidder.balance = bidder.balance - bid.value;
+   grain.state = 'SOLD';
+   grain.owner = bidder;
+   bid.state = 'SOLD';
+  
+  const grainRegistry = await getAssetRegistry('org.graindiscovery.Grain');
+     await grainRegistry.update(grain);
+  
+  const bidRegistry = await getAssetRegistry('org.graindiscovery.Bid');
+     await grainRegistry.update(bid);
+  
+  const userRegistry = await getParticipantRegistry('org.graindiscovery.Member');
+     await userRegistry.update(bidder);
+     await userRegistry.update(grainSeller); 
+  
+}
+
